18f56d46a313e321fba119d995eded9bb287f5f1
x80486 <masked.github@mailnull.com>
2020-07-29 11:12:25 -0400

    fix: remove JAVA_TOOL_OPTIONS environment variable

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,9 +4,7 @@ set -o errexit
 
 IDEA_JDK="/app/extra/idea-IU/jre64"
 export IDEA_JDK
-JAVA_TOOL_OPTIONS="-Djava.io.tmpdir=${XDG_CACHE_HOME}/tmp"
-export JAVA_TOOL_OPTIONS
-TMPDIR="${XDG_CACHE_HOME}/tmp"
+TMPDIR="${XDG_RUNTIME_DIR}/app/${FLATPAK_ID}"
 export TMPDIR
 PATH="${PATH}:${EXTRA_PATH}"
 export PATH
