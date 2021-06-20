14a6d80d445b86da22a2aa6ebd13f7b88076ab43
Jackson Dearnley <jackson.dearnley@gmail.com>
2021-06-20 08:41:36 -0400

    feat: add support for discord integration

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,4 +9,9 @@ export TMPDIR
 PATH="${PATH}:${EXTRA_PATH}"
 export PATH
 
+# https://plugins.jetbrains.com/plugin/10233-discord-integration
+for i in {0..9}; do
+    test -S $XDG_RUNTIME_DIR/discord-ipc-$i || ln -sf {app/com.discordapp.Discord,$XDG_RUNTIME_DIR}/discord-ipc-$i;
+done
+
 exec env /app/extra/idea-IU/bin/idea.sh "$@"
