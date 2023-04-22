eff6ce3469e712bf195ae6515b920400eb41ba19
x80486 <gfd2zs93@duck.com>
2023-04-22 08:22:37 -0400

    Integrate and enable jetbrains-flatpak-wrapper

--- apply_extra.sh
+++ apply_extra.sh
@@ -2,6 +2,6 @@
 
 set -o errexit
 
-install --directory --mode=0755 idea-IU/
-tar --directory=idea-IU/ --extract --file=ideaIU.tar.gz --gunzip --strip-components=1
+install --directory --mode=0755 IDEA-U/
+tar --directory=IDEA-U/ --extract --file=ideaIU.tar.gz --gunzip --strip-components=1
 rm --force ideaIU.tar.gz
