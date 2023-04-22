6025789e9751539ef3a6e3baf0f52b051a8d3f39
x80486 <gfd2zs93@duck.com>
2023-04-22 07:56:00 -0400

    Remove zypak-wrapper integration

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,7 +11,7 @@ export PATH
 
 # https://plugins.jetbrains.com/plugin/10233-discord-integration
 for i in {0..9}; do
-    test -S $XDG_RUNTIME_DIR/discord-ipc-$i || ln -sf {app/com.discordapp.Discord,$XDG_RUNTIME_DIR}/discord-ipc-$i;
+  test -S $XDG_RUNTIME_DIR/discord-ipc-$i || ln -sf {app/com.discordapp.Discord,$XDG_RUNTIME_DIR}/discord-ipc-$i;
 done
 
-exec env zypak-wrapper /app/extra/idea-IU/bin/idea.sh "$@"
+exec env /app/extra/idea-IU/bin/idea.sh "$@"
