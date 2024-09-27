50dce959c1c00f18c932e41baaf15d1d895f857d
x80486 <gfd2zs93@duck.com>
2024-09-27 10:22:33 -0400

    Embed the extra_data shell script in the Flatpak manifest

--- apply_extra.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env sh
-
-set -o errexit
-
-install --directory --mode=0755 IDEA-U/
-tar --directory=IDEA-U/ --extract --file=ideaIU.tar.gz --gunzip --strip-components=1
-rm --force ideaIU.tar.gz
